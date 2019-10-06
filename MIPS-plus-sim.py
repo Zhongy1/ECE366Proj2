5b248608931e03d506390f7142d777b365e36619
Zhongyi Chen <zhongyi753@gmail.com>
2019-10-06 13:30:26 -0500

    implement addi

--- MIPS-plus-sim.py
+++ MIPS-plus-sim.py
@@ -1,9 +1,37 @@
 
+registers = {
+        '$0': 0,
+        '$8': 0,
+        '$9': 0,
+        '$10': 0,
+        '$11': 0,
+        '$12': 0,
+        '$13': 0,
+        '$14': 0,
+        '$15': 0,
+        '$16': 0,
+        '$17': 0,
+        '$18': 0,
+        '$19': 0,
+        '$20': 0,
+        '$21': 0,
+        '$22': 0,
+        '$23': 0,
+        'lo': 0,
+        'hi': 0,
+        'pc': 0
+    }
+
+    #Each element in array represents a 4 byte chunk (32 bits)
+    #Starts at memory location 0x2000 and ends at 0x3000
+    memory = [0] * 1024
+
 def lui():
 
 def ori():
 
-def addi():
+def addi(reg1, reg2, imm):
+    registers[reg1] = registers[reg2] + imm
 
 def multu():
 
@@ -48,36 +76,9 @@ class Instruction:
     }
 
 def main():
-    registers = {
-        '$0': 0,
-        '$8': 0,
-        '$9': 0,
-        '$10': 0,
-        '$11': 0,
-        '$12': 0,
-        '$13': 0,
-        '$14': 0,
-        '$15': 0,
-        '$16': 0,
-        '$17': 0,
-        '$18': 0,
-        '$19': 0,
-        '$20': 0,
-        '$21': 0,
-        '$22': 0,
-        '$23': 0,
-        'lo': 0,
-        'hi': 0,
-        'pc': 0
-    }
-
-    #Each element in array represents a 4 byte chunk (32 bits)
-    #Starts at memory location 0x2000 and ends at 0x3000
-    memory = [0] * 1024
-    labelDict = {}
-
     f = open("output.txt","w+")
     h = open("testcase.asm","r")
+    labelDict = {}
     asm = h.readlines()
     storeLabels(labelDict, asm)
     # From previous homework, not to be used
@@ -86,7 +87,7 @@ def main():
     #     string = instr.toString()
     #     if string != 'InvalidInstruction':
     #         f.write(hex(int(string,2)) + '\n');
-    
+
 
 
 if __name__ == "__main__":
